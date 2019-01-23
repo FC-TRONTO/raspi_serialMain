2483b9eac9a663c6fc6fc2a3d4bac0d538b17373
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-01-23 21:52:38 +0900

    Update: read process

--- serialCom.py
+++ serialCom.py
@@ -4,17 +4,18 @@ import serial
 import time
 
 def receiveDataLoop(shmem):
+    # USBポートを使い、シリアル通信開始
     ev3_serial = serial.Serial('/dev/ttyUSB0', 115200, timeout=10)
     print(ev3_serial.portstr)
-    count = 0
     while 1:
         read_data = ev3_serial.readline()
-        print(read_data)
-        print(shmem.irAngle)
-        print(shmem.uSonicDis)
-        shmem.irAngle = count
-        count += 1
-        time.sleep(2)
+        # 読み込んだ文字列を,区切りでリストに変換
+        sensorValues = read_data.strip().split(",")
+        # 共有メモリの値を更新
+        shmem.irAngle = int(sensorValues[0])
+        shmem.uSonicDis = float(sensorValues[1])
+        # 100msごとに実行
+        time.sleep(0.1)
 
 def target(shmem):
     receiveDataLoop(shmem)
